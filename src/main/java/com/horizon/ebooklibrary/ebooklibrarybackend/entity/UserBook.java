d8c269fafef9a659c987df10123d57c402baa70e
Horizon Halifa <OrHalifa1@gmail.com>
2025-04-19 13:26:22 +0300

    Documentation

--- src/main/java/com/horizon/ebooklibrary/ebooklibrarybackend/entity/UserBook.java
+++ src/main/java/com/horizon/ebooklibrary/ebooklibrarybackend/entity/UserBook.java
@@ -13,6 +13,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Entity representing the relationship between a User and a Book,
+ * including the user's reading status for that specific book.
+ * This allow sus to track which books each user has read or not.
+ */
 @Entity
 @Table(name = "user_books")
 @Getter
@@ -22,16 +27,30 @@ import lombok.Setter;
 @Builder
 public class UserBook {
 
+    /**
+     * Unique ID for this user-book relationship
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * The User who owns this book relationship.
+     * Many UseBook entries can point to the same User.
+     */
     @ManyToOne(optional = false)
     private User user;
 
+    /**
+     * The Book that is being tracked for the user.
+     * Many UserBook entries can point to the same Book.
+     */
     @ManyToOne(optional = false)
     private Book book;
 
+    /**
+     * Whether this User has marked the book as read.
+     */
     @Column(nullable = false)
     private boolean read;
 }
