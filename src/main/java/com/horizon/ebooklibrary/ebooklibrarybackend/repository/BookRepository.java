cd950eb35adff943b4a3b1d7792e03b75c3226b5
Horizon Halifa <OrHalifa1@gmail.com>
2025-05-03 16:37:43 +0300

    Deleted methods used for testing only

--- src/main/java/com/horizon/ebooklibrary/ebooklibrarybackend/repository/BookRepository.java
+++ src/main/java/com/horizon/ebooklibrary/ebooklibrarybackend/repository/BookRepository.java
@@ -5,11 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import com.horizon.ebooklibrary.ebooklibrarybackend.entity.Book;
 
-import java.util.List;
-
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    List<Book> findByReadTrue();
-    List<Book> findByReadFalse();
 }
