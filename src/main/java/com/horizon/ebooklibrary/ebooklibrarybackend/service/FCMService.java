e900b74ecd8282750be2ae1c88aa40fb12894644
Horizon Halifa <OrHalifa1@gmail.com>
2025-05-09 17:21:24 +0300

    Removed per device notification and added support for sending push notifications to topics

--- src/main/java/com/horizon/ebooklibrary/ebooklibrarybackend/service/FCMService.java
+++ src/main/java/com/horizon/ebooklibrary/ebooklibrarybackend/service/FCMService.java
@@ -3,6 +3,7 @@ package com.horizon.ebooklibrary.ebooklibrarybackend.service;
 import com.google.firebase.messaging.AndroidConfig;
 import com.google.firebase.messaging.AndroidNotification;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
 
@@ -16,33 +17,23 @@ import org.springframework.stereotype.Service;
 public class FCMService {
 
     /**
-     * Sends a push notification to a specific device using it's FCM token.
-     * @param targetToken FCM registration token of the target device
-     * @param title Title of the notification
-     * @param body Body text of the notification
-     * @throws Exception if message sending fails
+     * Send a push notification to a given topic
+     * @param topic the FCM topic (without /topics/ prefix)
+     * @param title the notification title
+     * @param body the notification body
+     * @throws FirebaseMessagingException if the send fails
      */
-    public void sendPushNotification(String targetToken, String title, String body) throws Exception {
-        // targetToken is the unique token each device receives from Firebase.
+    public void sendToTopic(String topic, String title, String body) throws FirebaseMessagingException {
         Message message = Message.builder()
-                .setToken(targetToken)
-
-                // Notification is the actual visual notification the user sees
+                .setTopic(topic)
                 .setNotification(Notification.builder()
                         .setTitle(title)
                         .setBody(body)
                         .build())
-
-                // AndroidConfig allows setting platform-specific behaviours (like notification sounds or click actions)
-                .setAndroidConfig(AndroidConfig.builder()
-                        .setNotification(AndroidNotification.builder()
-                                .setSound("default")
-                                .setClickAction("OPEN_BOOK_LIST")
-                                .build())
-                        .build())
                 .build();
 
         String response = FirebaseMessaging.getInstance().send(message);
-        System.out.println("Successfully sent message: " + response);
+        System.out.println("Sent to topic: " + response);
     }
+
 }
